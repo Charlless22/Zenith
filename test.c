53982a92b79272e6f71620a8fa5a10cdd9c4d3ba
Charles Delachapelle <charles.delachapelle@epitech.eu>
2024-10-04 00:29:46 +0200

    commit

---
+++ test.c
@@ -0,0 +1,28 @@
+int a = 10;
+a = 10;
+a == 20
+const int a = 10;
+
+int == chiffre
+char == caractere 'c'
+float == chiffre decimal
+
+pointeurs
+
+int * = [1, 2, 3]
+char * = 'je suis ici'
+float *
+
+while
+for
+
+int i = 0;
+int *index = [0, 1, 2, 3];
+char *tableau = 'je suis ici'
+
+for (int i = 0; tableau[i] != NULL; i++) {
+    write(tableau[i]);
+}
+
+
+char ** = [['je suis ici', 'je suis pas ici']]
